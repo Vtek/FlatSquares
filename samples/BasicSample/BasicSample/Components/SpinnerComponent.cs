77c9376aa33ab36518048b2011555f0951f9349b
pontoreau <pontoreau.sylvain@gmail.com>
2017-03-18 21:42:42 +0100

    Touch provider using

--- samples/BasicSample/BasicSample/Components/SpinnerComponent.cs
+++ samples/BasicSample/BasicSample/Components/SpinnerComponent.cs
@@ -1,15 +1,21 @@
 ﻿using FlatSquares;
 using FlatSquares.Common;
 using FlatSquares.Core;
+using FlatSquares.Providers;
 
 namespace BasicSample
 {
     public class SpinnerComponent : Component, IUpdate
     {
+        public ITouchProvider TouchProvider { get; set; }
+
         public float Speed { get; set; }
 
         public void Update(float elapsed)
         {
+            if(TouchProvider.Up(0))
+                Node.Position = TouchProvider.GetPosition(0).Value;
+
             Node.Rotation += elapsed;
             Node.Rotation = Node.Rotation % (MathHelper.Pi * 2);
         }
