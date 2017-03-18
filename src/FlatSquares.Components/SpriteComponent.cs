f0d0c622538273b5847c966e920d7dd7d8aa7bc0
pontoreau <pontoreau.sylvain@gmail.com>
2017-03-18 15:17:43 +0100

    Add documentation

--- src/FlatSquares.Components/SpriteComponent.cs
+++ src/FlatSquares.Components/SpriteComponent.cs
@@ -4,18 +4,62 @@ using FlatSquares.Providers;
 
 namespace FlatSquares.Components
 {
+    /// <summary>
+    /// Sprite component.
+    /// </summary>
     public class SpriteComponent : Component, IRender, ILoad
     {
+        /// <summary>
+        /// Gets or sets the content provider.
+        /// </summary>
+        /// <value>The content provider.</value>
         public IContentProvider ContentProvider { get; set; }
 
+        /// <summary>
+        /// Gets or sets the origin.
+        /// </summary>
+        /// <value>The origin.</value>
         public Vector Origin { get; set; } = Vector.Zero;
+
+        /// <summary>
+        /// Gets the position.
+        /// </summary>
+        /// <value>The position.</value>
         public Vector Position => Node.Position;
+
+        /// <summary>
+        /// Gets the rotation.
+        /// </summary>
+        /// <value>The rotation.</value>
         public float Rotation => Node.Rotation;
+
+        /// <summary>
+        /// Gets the scale.
+        /// </summary>
+        /// <value>The scale.</value>
         public float Scale => Node.Scale;
+
+        /// <summary>
+        /// Gets or sets the source.
+        /// </summary>
+        /// <value>The source.</value>
         public Rectangle Source { get; set; }
+
+        /// <summary>
+        /// Gets or sets the texture path.
+        /// </summary>
+        /// <value>The texture path.</value>
         public string TexturePath { get; set; }
+
+        /// <summary>
+        /// Gets the texture.
+        /// </summary>
+        /// <value>The texture.</value>
         public object Texture { get; private set; }
 
+        /// <summary>
+        /// Load this instance.
+        /// </summary>
         public void Load() => Texture = ContentProvider.Load(TexturePath);
     }
 }
--- src/FlatSquares.MonoGame/Providers/ContentProvider.cs
+++ src/FlatSquares.MonoGame/Providers/ContentProvider.cs
@@ -3,13 +3,22 @@ using Microsoft.Xna.Framework.Content;
 
 namespace FlatSquares.MonoGame.Providers
 {
+    /// <summary>
+    /// Content provider.
+    /// </summary>
     class ContentProvider : IContentProvider
     {
+        /// <summary>
+        /// Gets or sets the content manager.
+        /// </summary>
+        /// <value>The content manager.</value>
         public ContentManager ContentManager { get; set; }
 
-        public object Load(string resourcePath)
-        {
-            return ContentManager.Load<object>(resourcePath);
-        }
+        /// <summary>
+        /// Load a resource.
+        /// </summary>
+        /// <returns>Resource.</returns>
+        /// <param name="resourcePath">Resource path.</param>
+        public object Load(string resourcePath) => ContentManager.Load<object>(resourcePath);
     }
 }
